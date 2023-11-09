11d33768be45ba996c29ac36e28b883ff8ce3bf5
oblsoun <ychyung0615@gmail.com>
2023-11-09 10:38:45 +0900

    #4 홀수 짝수 판별 조건연산자 사용

--- Java/src/ch02/project02.java
+++ Java/src/ch02/project02.java
@@ -6,16 +6,14 @@ import java.util.Scanner;
 public class project02 {
 	public static void main(String[] args) {
 		int n;
+		String result;
 		
 		Scanner in = new Scanner(System.in);
 		
 		System.out.print("정수를 입력하세요: ");
 		n = in.nextInt();
 		
-		if (n % 2 == 0) {
-			System.out.println("짝수");
-		} else {
-			System.out.println("홀수");
-		}
+		result = (n % 2 == 0) ? "짝수" : "홀수";
+		System.out.println(result);
 	}
 }
